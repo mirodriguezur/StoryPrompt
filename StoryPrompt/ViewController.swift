965f69bfa5a82e58795c564eb0a6a72816df2ce0
Michael Rodriguez <miarodriguezur@unal.edu.co>
2021-10-27 00:23:41 -0500

    start project

---
+++ StoryPrompt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by Michael Alexander Rodriguez Urbina on 27/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
